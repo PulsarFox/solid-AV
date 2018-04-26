c0b0586b3b2f2a28edb8379f877a0add5b580aeb
Pulsarfox <samy.vincentffs@gmail.com>
2018-04-26 12:28:51 +0200

    Revert changes because mount on another slot is way too buggy

--- init.js
+++ init.js
@@ -7,8 +7,11 @@ var request = require('request')
 var util = require('util')
 
 var port = 3000
-/*
 
+var privateKey  = fs.readFileSync('./localhost.key', 'utf8');
+var certificate = fs.readFileSync('./localhost.crt', 'utf8');
+
+let credentials = {key: privateKey, cert: certificate};
 let app = express();
 
 let solidApp = solid({
@@ -21,7 +24,7 @@ let solidApp = solid({
     secret: 'node-ldp', // Express Session secret key
     sslCert: './localhost.crt', // Path to the ssl cert 
     sslKey: './localhost.key', // Path to the ssl key
-    mount: '/Ldp', // Where to mount Linked Data Platform
+    mount: '/', // Where to mount Linked Data Platform
     webid: true, // Enable WebID+TLS+OIDC authentication
     suffixAcl: '.acl', // Suffix for acl files
     corsProxy: false, // Where to mount the CORS proxy 
@@ -38,33 +41,6 @@ var server = https.createServer(credentials, app)
 server.listen(port, function(){ 
     console.log("launsch on port " + port);
 })
-*/
-let mount = "/Ldp"
-let Uri = "https://localhost:" + port + mount
-
-var ldp = solid.createServer({
-    cache: 0, // Set cache time (in seconds), 0 for no cache
-    serverUri:Uri,
-    live: true, // Enable live support through WebSockets
-    root: './.data', // Root location on the filesystem to serve resources
-    configPath:'./.config',
-    dbPath:'./.db',
-    secret: 'node-ldp', // Express Session secret key
-    sslCert: './localhost.crt', // Path to the ssl cert 
-    sslKey: './localhost.key', // Path to the ssl key
-    mount: '/Ldp', // Where to mount Linked Data Platform
-    webid: true, // Enable WebID+TLS+OIDC authentication
-    suffixAcl: '.acl', // Suffix for acl files
-    corsProxy: false, // Where to mount the CORS proxy 
-    errorHandler: function(err, req, res, next){
-        console.log(err)
-    }, // function(err, req, res, next) to have a custom error handler
-    errorPages: false // specify a path where the error pages are})
-})
-
-ldp.listen(3000, function(){
-    console.log("launched on port " + port)
-})
 
 /*
 request({
@@ -77,10 +53,37 @@ request({
         'Link':'<http://www.w3.org/ns/ldp#Resource>; rel="type"',
         'Slug':'test1'
     },
-    uri: Uri+'/public/test/test2.ttl',
+    uri: 'https://localhost:3000/public/test/test2.ttl',
     body:"Hello",
     function(err, res, req, body){
         if (err)
             console.log("post err " + err)
     }
 })*/
+
+
+/*
+var ldp = solid.createServer({
+    cache: 0, // Set cache time (in seconds), 0 for no cache
+    serverUri:"https://localhost:"+port,
+    live: true, // Enable live support through WebSockets
+    root: './.data', // Root location on the filesystem to serve resources
+    configPath:'./.config',
+    dbPath:'./.db',
+    secret: 'node-ldp', // Express Session secret key
+    sslCert: './localhost.crt', // Path to the ssl cert 
+    sslKey: './localhost.key', // Path to the ssl key
+    mount: '/', // Where to mount Linked Data Platform
+    webid: true, // Enable WebID+TLS+OIDC authentication
+    suffixAcl: '.acl', // Suffix for acl files
+    corsProxy: false, // Where to mount the CORS proxy 
+    errorHandler: function(err, req, res, next){
+        console.log(err)
+    }, // function(err, req, res, next) to have a custom error handler
+    errorPages: false // specify a path where the error pages are})
+})
+
+ldp.listen(3000, function(){
+    console.log("launched on port " + port)
+})
+*/
