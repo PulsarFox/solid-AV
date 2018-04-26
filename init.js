c0c4a0d5ec5d66e38f55d61e30ad10331e8adfad
Pulsarfox <samy.vincentffs@gmail.com>
2018-04-26 12:18:41 +0200

    Mount on Ldp and templates changes, need to change auth-request.js

--- init.js
+++ init.js
@@ -7,11 +7,8 @@ var request = require('request')
 var util = require('util')
 
 var port = 3000
+/*
 
-var privateKey  = fs.readFileSync('./localhost.key', 'utf8');
-var certificate = fs.readFileSync('./localhost.crt', 'utf8');
-
-let credentials = {key: privateKey, cert: certificate};
 let app = express();
 
 let solidApp = solid({
@@ -24,7 +21,7 @@ let solidApp = solid({
     secret: 'node-ldp', // Express Session secret key
     sslCert: './localhost.crt', // Path to the ssl cert 
     sslKey: './localhost.key', // Path to the ssl key
-    mount: '/', // Where to mount Linked Data Platform
+    mount: '/Ldp', // Where to mount Linked Data Platform
     webid: true, // Enable WebID+TLS+OIDC authentication
     suffixAcl: '.acl', // Suffix for acl files
     corsProxy: false, // Where to mount the CORS proxy 
@@ -41,27 +38,13 @@ var server = https.createServer(credentials, app)
 server.listen(port, function(){ 
     console.log("launsch on port " + port);
 })
+*/
+let mount = "/Ldp"
+let Uri = "https://localhost:" + port + mount
 
-var options = {
-    url: "https://localhost:3000/test/test2.ttl",
-    "rejectUnauthorized": false,
-    headers:{
-        'Content-Type':'text/turtle',
-        'Link':'<http://www.w3.org/ns/ldp#Resource>; rel="type"',
-        'Slug':'test1'
-    }
-}
-
-request.options(options, function(err, res, req, body){
-    if (err)
-        console.log("post err " + err)
-})
-
-
-/*
 var ldp = solid.createServer({
     cache: 0, // Set cache time (in seconds), 0 for no cache
-    serverUri:"https://localhost:"+port,
+    serverUri:Uri,
     live: true, // Enable live support through WebSockets
     root: './.data', // Root location on the filesystem to serve resources
     configPath:'./.config',
@@ -69,7 +52,7 @@ var ldp = solid.createServer({
     secret: 'node-ldp', // Express Session secret key
     sslCert: './localhost.crt', // Path to the ssl cert 
     sslKey: './localhost.key', // Path to the ssl key
-    mount: '/', // Where to mount Linked Data Platform
+    mount: '/Ldp', // Where to mount Linked Data Platform
     webid: true, // Enable WebID+TLS+OIDC authentication
     suffixAcl: '.acl', // Suffix for acl files
     corsProxy: false, // Where to mount the CORS proxy 
@@ -82,4 +65,22 @@ var ldp = solid.createServer({
 ldp.listen(3000, function(){
     console.log("launched on port " + port)
 })
-*/
+
+/*
+request({
+    method: 'PUT',
+    preambleCRLF: true,
+    postambleCRLF: true,
+    "rejectUnauthorized": false,
+    headers:{
+        'Content-Type':'text/turtle',
+        'Link':'<http://www.w3.org/ns/ldp#Resource>; rel="type"',
+        'Slug':'test1'
+    },
+    uri: Uri+'/public/test/test2.ttl',
+    body:"Hello",
+    function(err, res, req, body){
+        if (err)
+            console.log("post err " + err)
+    }
+})*/
